79b7bfbd4a8e97462d31d53d39116c2a58735b08
SanjaBozic <my.life.my.music.my.dream@gmail.com>
2025-07-15 22:41:51 +0200

    fixed type issue in expenses grid

--- src/components/expenses/expenses-grid.tsx
+++ src/components/expenses/expenses-grid.tsx
@@ -1,15 +1,15 @@
-import { Button, Input, Popconfirm, Space, Table, Tag, type InputRef, type TableColumnsType, type TableColumnType } from "antd";
+import { Button, Form, Input, InputNumber, Popconfirm, Space, Table, Tag, type GetRef, type InputRef, type TableColumnType } from "antd";
 import useData from "../../hooks/use-data";
-import { useRef, useState } from "react";
-import type { FilterDropdownProps } from "antd/es/table/interface";
+import { createContext, useContext, useEffect, useRef, useState } from "react";
+import type { ColumnsType, FilterDropdownProps } from "antd/es/table/interface";
 import { SearchOutlined } from "@ant-design/icons";
 // @ts-ignore
 import Highlighter from 'react-highlight-words'
 
 interface DataType {
   key: string;
-  date: Date;
   description: string;
+  date: Date;
   amount: number;
   categories: string[];
 }
@@ -18,6 +18,18 @@ interface ExpensesGridProps {
   setLocalData: React.Dispatch<React.SetStateAction<DataType[]>>;
   keyLocalData: string;
   updateData: (newData: any[], keyLocalData: string, setLocalData: React.Dispatch<any>) => void;
+}
+
+interface EditableRowProps {
+  index: number;
+}
+interface EditableCellProps {
+  title: React.ReactNode;
+  editable: boolean;
+  dataIndex: keyof DataType;
+  record: DataType;
+  inputType: 'number' | 'text' | 'date';
+  handleSave: (record: DataType) => void;
 }
 
 function ExpensesGrid(props: ExpensesGridProps) {
@@ -150,12 +162,13 @@ function ExpensesGrid(props: ExpensesGridProps) {
 
     //END OF SEARCH FILTER
 
-    const columns: TableColumnsType<DataType> = [
+    const defaultColumns: (ColumnsType<DataType>[number] & { editable?: boolean })[] = [
         {
-            title: 'Description',
+            title: "Description",
             dataIndex: "description",
             key: "description",
-            ...getColumnSearchProps("description")
+            ...getColumnSearchProps("description"),
+            editable: true
         },
         {
             title: "Date",
@@ -169,7 +182,8 @@ function ExpensesGrid(props: ExpensesGridProps) {
             title: "Amount (EUR)",
             dataIndex: "amount",
             key: "amount",
-            sorter: (a, b) => a.amount - b.amount
+            sorter: (a, b) => a.amount - b.amount,
+            editable: true
         },
         {
             title: "Categories",
@@ -182,6 +196,7 @@ function ExpensesGrid(props: ExpensesGridProps) {
         },
         {
             title: "Action",
+            dataIndex: "action",
             width: 50,
             key: "action",
             render: (_: any) => ( props.localData.length >= 1 ? (
@@ -193,9 +208,120 @@ function ExpensesGrid(props: ExpensesGridProps) {
         }
     ]
 
+    //START OF EDIT MODE
+
+    type FormInstance<T> = GetRef<typeof Form<T>>;
+    const EditableContext = createContext<FormInstance<any> | null>(null);
+
+    const EditableRow: React.FC<EditableRowProps> = ({ index, ...props }) => {
+        const [form] = Form.useForm();
+        return (
+            <Form form={form} component={false}>
+            <EditableContext.Provider value={form}>
+                <tr {...props} />
+            </EditableContext.Provider>
+            </Form>
+        );
+    };
+
+    const EditableCell: React.FC<React.PropsWithChildren<EditableCellProps>> = ({ title, editable, children, inputType, dataIndex, record, handleSave, ...restProps }) => {
+        const [editing, setEditing] = useState(false);
+        const inputRef = useRef<InputRef>(null);
+        const form = useContext(EditableContext)!;
+
+        useEffect(() => {
+            if (editing) {
+            inputRef.current?.focus();
+            }
+        }, [editing]);
+
+        const toggleEdit = () => {props.localData.length + 1,
+            setEditing(!editing);
+            form.setFieldsValue({ [dataIndex]: record[dataIndex] });
+        };
+
+        const save = async () => {
+            try {
+            const values = await form.validateFields();
+
+            toggleEdit();
+            handleSave({ ...record, ...values });
+            } catch (errInfo) {
+            console.log('Save failed:', errInfo);
+            }
+        };
+
+        let childNode = children;
+
+        if (editable) {
+            const inputNode = inputType === 'number' ? <InputNumber onPressEnter={save} onBlur={save}/> : <Input ref={inputRef} onPressEnter={save} onBlur={save}/>;
+            childNode = editing ? (
+            <Form.Item
+                style={{ margin: 0 }}
+                name={dataIndex}
+                rules={[{ required: true, message: `${title} is required.` }]}
+            >
+                {inputNode}
+            </Form.Item>
+            ) : (
+            <div
+                className="editable-cell-value-wrap"
+                style={{ paddingInlineEnd: 24 }}
+                onClick={toggleEdit}
+            >
+                {children}
+            </div>
+            );
+        }
+
+        return <td {...restProps}>{childNode}</td>;
+    };
+
+    const components = {
+        body: {
+        row: EditableRow,
+        cell: EditableCell,
+        },
+    };
+
+    const columns = defaultColumns.map((col) => {
+        if (!col.editable) {
+        return col;
+        }
+
+        if ('dataIndex' in col) {
+            return {
+                ...col,
+                onCell: (record: DataType) => ({
+                    record,
+                    inputType: col.dataIndex === 'amount' ? 'number' : 'text',
+                    editable: col.editable,
+                    dataIndex: col.dataIndex,
+                    title: col.title,
+                    handleSave,
+                }),
+            };
+        }
+
+        return col;
+    });
+
+    const handleSave = (row: DataType) => {
+        const newData = [...props.localData];
+        const index = newData.findIndex((item) => row.key === item.key);
+        const item = newData[index];
+        newData.splice(index, 1, {
+        ...item,
+        ...row,
+        });
+        props.updateData(newData, props.keyLocalData, props.setLocalData);
+    };
+
+    //END OF EDIT MODE
+
     return (
     <>
-        <Table<DataType> dataSource={data} pagination={{ pageSize: 10 }} columns={columns} style={{ minWidth: 600, width: '100%' }} ></Table>
+        <Table<DataType> dataSource={data} components={components} pagination={{ pageSize: 10 }} columns={columns as ColumnsType<DataType>} style={{ minWidth: 600, width: '100%' }} ></Table>
     </>
     )
 }
